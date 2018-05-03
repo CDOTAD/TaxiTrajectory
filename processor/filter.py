a304404b1257a2ca9f8d9544bffe8444e9500d5f
CDOTAD <923038023@qq.com>
2018-05-03 17:00:40 +0800

    fix some problems

--- processor/filter.py
+++ processor/filter.py
@@ -10,7 +10,7 @@ from shapely.geometry import LineString
 def near_point_combine(trajectory):
     fix_bit = True
 
-    min_dis = 20
+    min_dis = 70
 
     point_num = 0
     point_list = []
@@ -23,6 +23,8 @@ def near_point_combine(trajectory):
         point_num += 1
 
     point_correct_list = []
+
+    print('point_lis',point_list)
 
     while fix_bit is True:
 
@@ -54,6 +56,7 @@ def near_point_combine(trajectory):
 
             point_list = point_correct_list
             point_correct_list = []
+    print(point_list)
     if len(point_list) < 2:
         return LineString()
 
@@ -62,9 +65,35 @@ def near_point_combine(trajectory):
     return line_correct
 
 
+# return LineString, distance_mean, distance_var, point_num
+"""
+def trajectory_statis(trajectory):
+
+    point_list=[]
+    dis_list=[]
+    for point in list(trajectory.coords):
+        p = Point(point[0],point[1])
+        point_list.append(p)
+
+    for i in range(len(point_list)-1):
+
+        point_now = point_list[i]
+        point_next = point_list[i+1]
+
+        dis_list.append(point_now.distance(point_next))
+
+    statistics = np.array(dis_list)
+
+    mean = statistics.mean()
+    var = statistics.var()
+    n = len(dis_list)
+
+    return LineString(point_list),mean,var,n
+"""
+
 # fix the error points
 # input data type trajectory : shapely.geometry.LineString
-def error_record_fix(trajectory):
+def error_record_fix(trajectory, avg_dis=None, var_dis=None):
 
     point_list = []
     point_num = 0
@@ -85,10 +114,68 @@ def error_record_fix(trajectory):
             distance_list.append(dis)
 
         statistics_array = np.array(distance_list)
+        if avg_dis is None:
+            avg_dis = statistics_array.mean()
+        if var_dis is None:
+            var_dis = 3*math.sqrt((point_num/(point_num - 1))*statistics_array.var())
 
-        avg_dis = statistics_array.mean()
-        var_dis = 3*math.sqrt(statistics_array.var())
+        print('avg_dis',avg_dis)
+        print('var_dis',var_dis)
 
+        print('dis_array',distance_list)
+
+        error_list =[]
+
+        for i in range(len(distance_list)):
+
+            if distance_list[i] > avg_dis + var_dis:
+
+                error_list.append(i)
+
+        print('error_list',error_list)
+
+        if len(error_list)==0:
+            if avg_dis > 1000:
+                return None
+
+        if len(error_list)>1:
+            for i in range(len(error_list)-1):
+
+                error_dis_now = error_list[i]
+                error_dis_next = error_list[i+1]
+
+                if error_dis_next == error_dis_now + 1:
+                    error_index = error_dis_next
+                    error_point = point_list[error_index]
+                    point_pre = point_list[error_index-1]
+                    point_next = point_list[error_index+1]
+
+                    point_correct = Point((point_pre.x + point_next.x)/2,(point_pre.y + point_pre.y)/2)
+
+                    point_list[error_dis_next] = point_correct
+        elif len(error_list) == 1:
+
+            if error_list[0] == 0:
+
+                error_point = point_list[0]
+                point_1 = point_list[1]
+                point_2 = point_list[2]
+
+                point_correct = Point(2*point_1.x - point_2.x, 2*point_1.y - point_2.y)
+
+                point_list[0] = point_correct
+            elif error_list[0] == len(point_list) - 1:
+
+                error_point = point_list[-1]
+                point_1 = point_list[-2]
+                point_2 = point_list[-3]
+
+                point_correct = Point(2*point_1.x - point_2.x, 2*point_1.y - point_2.y)
+
+                point_list[-1] = point_correct
+
+
+        """
         for i in range(1,point_num - 1):
             point_now = point_list[i]
             point_pre = point_list[i-1]
@@ -115,7 +202,7 @@ def error_record_fix(trajectory):
                 point_correct = Point(2*point_now.x - point_pre.x,2*point_now.y - point_pre.y)
 
                 point_list[i+1] = point_correct
-
+            """
     line_correct = LineString(point_list)
     #print('fix',str(line_correct))
 
